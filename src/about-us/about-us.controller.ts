fc451d67f2bcdb5b5b7ed331db30c76633d96543
RafikMuradyan <muradyanrafik1@gmail.com>
2024-07-12 00:12:53 +0400

    added multer

--- src/about-us/about-us.controller.ts
+++ src/about-us/about-us.controller.ts
@@ -1,7 +1,36 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
 import { AboutUsService } from './about-us.service';
+import { diskStorage } from 'multer';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { v4 as uuidv4 } from 'uuid';
+import { AboutUsDto } from './dto/create-about-us.dto';
+import { AboutUs } from './schemas/about-us.schema';
+
+const storage = diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'dist/uploads/');
+  },
+  filename: (req, file, cb) => {
+    cb(null, `${uuidv4()}-${file.originalname}`);
+  },
+});
 
 @Controller('about-us')
 export class AboutUsController {
   constructor(private aboutUsService: AboutUsService) {}
+
+  @Post('')
+  @UseInterceptors(FileInterceptor('file', { storage }))
+  async yesim(
+    @UploadedFile() file,
+    @Body() aboutUs: AboutUsDto,
+  ): Promise<AboutUs> {
+    return this.aboutUsService.create(aboutUs, file.filename);
+  }
 }
--- src/about-us/about-us.service.ts
+++ src/about-us/about-us.service.ts
@@ -1,11 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { AboutUs } from './schemas/about-us.schema';
+import { AboutUsDto } from './dto/create-about-us.dto';
 
 @Injectable()
 export class AboutUsService {
   constructor(
     @InjectModel(AboutUs.name) private aboutUsModel: Model<AboutUs>,
   ) {}
+
+  async create(aboutUs: AboutUsDto, filename: string) {
+    const existingData = await this.aboutUsModel.findOne({
+      title: aboutUs.title,
+    });
+
+    if (existingData) {
+      throw new ConflictException('Data with this title already exists');
+    }
+
+    const createdData = new this.aboutUsModel({
+      image: filename,
+      ...aboutUs,
+    });
+
+    return createdData.save();
+  }
 }
