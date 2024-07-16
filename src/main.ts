7cf7d4e01a0f0c25a25fe6fdf0d7a2d9d540068d
RafikMuradyan <muradyanrafik1@gmail.com>
2024-07-16 21:58:44 +0400

    added uploads folder

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,23 @@
-import { NestFactory } from "@nestjs/core";
-import type { NestExpressApplication } from "@nestjs/platform-express";
-import { ValidationPipe } from "@nestjs/common";
-import { AppModule } from "./app.module";
+import { NestFactory } from '@nestjs/core';
+import type { NestExpressApplication } from '@nestjs/platform-express';
+import { ValidationPipe } from '@nestjs/common';
+import { AppModule } from './app.module';
+import { existsSync, mkdirSync } from 'fs';
+import { join } from 'path';
 
 async function bootstrap(): Promise<NestExpressApplication> {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
     cors: true,
   });
 
-  app.setGlobalPrefix("api");
+  const uploadDir = join(__dirname, 'uploads');
+
+  if (!existsSync(uploadDir)) {
+    mkdirSync(uploadDir, { recursive: true });
+    console.log('Uploads directory created');
+  }
+
+  app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
 
   const port = process.env.PORT || 4000;
